9e54d77999d64b41f26137b3ea2a6e0e3e6994aa
jyrgalka <jikujakypova>
2025-05-02 23:26:45 +0600

    Добавил index.html

--- my_bot.py
+++ my_bot.py
@@ -65,6 +65,8 @@ def get_text_message(message):
         bot.send_message(message.from_user.id, text='Выбери свой знак зодиака', reply_markup=keyboard)
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /Start.")
+
+
 # Обработчик нажатий на кнопки
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
